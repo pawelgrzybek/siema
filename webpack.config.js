3179e2dc6dd2b18c8dbd77b8c10f47d6b12373cd
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2017-04-08 08:14:06 +0100

    Enforce good styling code by implementing ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,12 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
