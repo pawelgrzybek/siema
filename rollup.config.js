26c69915468d5d58e3ef52f22801dee23513beeb
Benny Powers <benny.p@blink.gg>
2018-05-07 12:05:14 +0300

    Move build to rollup, create es module build, add sourcemaps

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import uglify from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
+
+const es5 = {
+  input: './src/siema.js',
+  plugins: [
+    babel(),
+    uglify(),
+  ],
+  output: {
+    file: 'dist/siema.min.js',
+    format: 'iife',
+    sourcemap: true,
+    name: 'Siema',
+  }
+};
+
+const es6 = {
+  input: './src/siema.js',
+  plugins: [
+    uglify(),
+  ],
+  output: {
+    file: 'dist/siema.es.js',
+    sourcemap: true,
+    format: 'es',
+  }
+};
+
+export default [es5, es6];
